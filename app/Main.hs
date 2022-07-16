e70dca576b51b41eee4508de5b913a57b1462a9b
magao02 <lucas.araripina@hotmail.com>
2022-07-16 11:14:48 -0300

    adicionando apostas randomicas

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
-import System.Random 
+import System.Random
 
 
 selecoes = ["1- Brasil", "2- Alemenha", "3- Argentina", "4- Belgica","5- Holanda", "6- Inglaterra", "7- Franca", "8- Espanha", "9- Portugal","10- Croacia", "11- Uruguai", "12- Dinamarca", "13- servia", "14- suica", "15- ira", "16- coreia do sul"]
-valorSelecao = [9.2, 9.3, 9.0, 9.2, 8.9, 8.8, 9.4, 8.7, 9.0, 8.5, 8.4,8.7,8.0,8.8,7.5,7.7]
+valorSelecao = [9.2, 9.3, 9.0, 9.2, 8.9, 8.8, 9.4, 8.7, 9.1, 8.5, 8.4,8.7,8.0,8.8,7.5,7.7]
 
 
 
@@ -13,7 +13,7 @@ printSelecoes x s = do
     verSelecoes(x+1) s
 
 verSelecoes x s = do
-    if x == (length selecoes) then main2 s
+    if x == length selecoes then main2 s
     else printSelecoes x s
 
 
@@ -24,12 +24,15 @@ verSaldo s =do
 
 
 apostar s = do
-    putStrLn "Time que voce vai apostar"
-    input2 <- getLine
-    let time1 = (read input2 :: Int)
+    print "Time que voce vai apostar"
+    time1 <- randomRIO (1,16 :: Int)
     putStrLn "Time adversário"
-    input3 <- getLine
-    let time2 = (read input3 :: Int)
+    time2 <- randomRIO (1,16 :: Int)
+    putStrLn "Time que voce vai apostar"
+    print (selecoes !! time1)
+    print (selecoes !! time2)
+    input2 <-getLine
+    let timeApostado = (read input2 :: Int)
     putStrLn "Valor da aposta"
     input5 <- getLine
     let valorAposta = (read input5 :: Int)
@@ -37,14 +40,42 @@ apostar s = do
         print "Saldo insuficiente"
         main2 s
         else do
-            if (valorSelecao !!(time1-1)) > (valorSelecao !!(time2-1)) then do
-                print "Voce ganhou"
-                main2 (s+valorAposta)
-                
-            else do print "Voce perdeu"
-                    main2 (s-valorAposta)
+            rodardados <- randomRIO (1,6 :: Int)
+            print rodardados
+            if (valorSelecao !! time1) > (valorSelecao !! time2) then do
+                if rodardados < 4 then do
+                    if timeApostado == 1 then do
+                        print "voce ganhou a aposta"
+                        main2 (s+valorAposta)
+                    else do
+                        print "voce perdeu a aposta"
+                        main2 (s - valorAposta)
+                else do
+                     if timeApostado == 2 then do
+                        print "voce ganhou a aposta"
+                        main2 (s+valorAposta)
+                     else do
+                        print "voce perdeu a aposta"
+                        main2 (s - valorAposta)
+            else do
+                if rodardados < 4 then do
+                    if timeApostado == 2 then do
+                        print "voce ganhou a aposta"
+                        main2 (s+valorAposta)
+                    else do
+                        print "voce perdeu a aposta"
+                        main2 (s - valorAposta)
+                else do
+                     if timeApostado == 1 then do
+                        print "voce ganhou a aposta"
+                        main2 (s+valorAposta)
+                     else do
+                        print "voce perdeu a aposta"
+                        main2 (s - valorAposta)
 
-    
+
+
+
 
 recarregar = do
     putStrLn "Digite o valor da recarga"
@@ -56,18 +87,19 @@ recarregar = do
 
 
 
-defineAcao x s = if x == 1 then verSelecoes 0 s
-              else if x == 2 then verSaldo s
-              else if x == 4 then apostar s
-              else if x == 3 then recarregar
-              else main2 s
-                
+defineAcao x s
+  | x == 1 = verSelecoes 0 s
+  | x == 2 = verSaldo s
+  | x == 4 = apostar s
+  | x == 3 = recarregar
+  | otherwise = main2 s
 
 
-main = do 
+
+main = do
     numeroAleatorio <- randomRIO (1,16 :: Int)
-    putStrLn $ show numeroAleatorio
-    putStrLn "Selecione:"  
+    print numeroAleatorio
+    putStrLn "Selecione:"
     putStrLn "1- ver selecoes"
     putStrLn "2- ver saldo"
     putStrLn "3- recarregar o saldo"
@@ -76,10 +108,10 @@ main = do
     let x = (read input1 :: Int)
     defineAcao x 20
 
-main2 s = do 
+main2 s = do
     numeroAleatorio <- randomRIO (1,16 :: Int)
-    putStrLn $ show numeroAleatorio
-    putStrLn "Selecione:"  
+    print numeroAleatorio
+    putStrLn "Selecione:"
     putStrLn "1- ver selecoes"
     putStrLn "2- ver saldo"
     putStrLn "3- recarregar o saldo"
