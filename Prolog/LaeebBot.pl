baa99848ad0157ce20ff6b04bebd68ada89aa985
Erick Araken Vieira Gomes <56178011+ErickBrth@users.noreply.github.com>
2022-08-18 10:36:48 -0300

    resolvendo erros e adicionando funções ver_saldo e recarregar

--- Prolog/LaeebBot.pl
+++ Prolog/LaeebBot.pl
@@ -1,27 +1,40 @@
 selecoes(R):- R = (["1- Brasil", "2- Alemenha", "3- Argentina", "4- Belgica",
                  "5- Holanda", "6- Inglaterra", "7- Franca", "8- Espanha", "9- Portugal",
                  "10- Croacia", "11- Uruguai", "12- Dinamarca", "13- servia", "14- suica",
-                 "15- ira", "16- coreia do sul"]),
-                 write(R).
+                 "15- ira", "16- coreia do sul"]).
                  
 randseq(K, N, List) :- /*lista com os valores das selecoes*/
       randset(16, 93, Set),
       random_permutation(Set, List).
 
 mostra_selecoes() :- 
-    selecoes(R).
+    selecoes(R),
+    write(R),nl,
+    main.
 
-saldo().
+ver_saldo(Saldo,NovoSaldo):- 
+    write("Seu saldo e: "),  
+    write(Saldo), nl,
+    main. 
 
-recarregar().
+recarregar(Saldo,NovoSaldo):-  
+    write("Digite o valor a ser recarregado: "),
+    read(Num),                        /*logica de recarregar aqui,chamo penas as funcoes de acordo com as opcoes escolhidas*/
+    NovoSaldo is Saldo + Num,
+    write(NovoSaldo),
+    main.  
 
-apostar().
+apostar(Saldo,NovoSaldo):-
+    write("Digite o time 1: "),
+    read(time1),
+    write("Digite o time 2: "),
+    read(time2).
 
-define_acao(Option) :-
-    (mostra_selecoes() -> Option =:= 1;
-    saldo() -> Option =:= 2;
-    recarregar() -> Option =:= 3;
-    apostar() -> Option =:= 4).
+define_acao(Option,Saldo,NovoSaldo) :-
+    (Option =:= 1 -> mostra_selecoes();
+    Option =:= 2 -> ver_saldo(Saldo,NovoSaldo);
+    Option =:= 3 -> recarregar(Saldo,NovoSaldo);
+    Option =:= 4 -> apostar(Saldo,NovoSaldo)).
 
 
 main:-
@@ -31,4 +44,5 @@ main:-
     write("3- recarregar o saldo"), nl,
     write("4- Apostar"), nl,
     read(Option),
-    define_acao(Option),halt.
+    Saldo is 20,
+    define_acao(Option,Saldo,NovoSaldo),halt.
